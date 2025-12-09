0a05a3e8ff46fa89dfcff42fbcbee5c5bdf56e0e
Jy654 <JNavarro69@cps.edu>
2025-12-09 15:30:14 +0000

    loops2

--- 2_while_loops.py
+++ 2_while_loops.py
@@ -1,6 +1,51 @@
+# while loop = execute some code WHILE some confition remains true
+
+# name = input ("Enter your name: ")
+
+# while name == "":
+#     print("You did not enter your name")
+#     name = input("Enter yout name: ")
+ 
+# print(f"Hello {name}")
+
+# age = int(input("Enter your age: "))
+
+
+# while age < 0 :
+#     print("age can't be negative")
+#     age = int(input("Enter your age: "))
+
+# print(f"You are {age} years old")
+
+
+# food = input ("Enter a food you like (q to quit): ")
+
+# while not food == "q":
+#     print(f"you like {food}")
+#     food = input("Enter another food you like (q to quit): ")
+
+# print("bye")
+
+# food = input("What is nyour favorite food? ")
+
+
+num = int(input("Enter a # between 1 - 10: "))
+
+while num < 1 or num > 10: 
+    print(f"{num} is not valid")
+    num = int(input("Enter a # between 1 - 10: "))
+
+print(f"your number is {num}")
+
 # Given:
 colors = ["red", "blue", "green", "yellow", "purple"]
 
 # Challenge:
 # Use a while loop to print each color UNTIL you find "yellow".
 # Do NOT print "yellow" — stop before it.
+index = 0 
+while index < len(colors):m 
+    if colors{index} == "yellow":
+        break
+    print(colors[index])
+    index += 1
